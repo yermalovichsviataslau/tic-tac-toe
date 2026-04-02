f7615f60fc1ba4171eb6baafb64ac5dab8be6d17
Yermalovich Sviataslau <sviatosla.ermolovich@gmail.com>
2026-04-02 16:46:54 +0300

    init: start project

---
+++ src/components/Board.jsx
@@ -0,0 +1,24 @@
+function Board (){
+  return(
+    <>
+      <div className="board-row">
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+
+      <div className="board-row">
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+      <div className="brow-row">
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+        <button className='square'>X</button>
+      </div>
+    </>
+  )
+}
+
+export default Board
